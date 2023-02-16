c13a60ec3d95c9d1ec8a840f46f6ee8ea757154d
Sheetal-BC <sheetalbc123@gmail.com>
2023-02-16 01:34:21 -0800

    updated App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+  import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Flex, Text } from "@chakra-ui/react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HeaderMob from "./components/mob/Header/HeaderMob";
 import HomeSc from "./screens/HomeSc";
 import CareersSc from "./screens/CareersSc";
 import Contact from "./screens/Contact";
@@ -50,10 +51,11 @@ function App() {
       setIsMobile(true);
     }
   },[]);
+  console.log("app",isMobile)
   if(!isMobile){
   return (
     <BrowserRouter>
-      <Header />
+      <Header/>
       <Flex
         as="main"
         direction="column"
@@ -91,7 +93,7 @@ function App() {
   else{
     return (
       <BrowserRouter>
-        {/* <Header /> */}
+        <HeaderMob/>
         <Flex
           as="main"
           direction="column"
